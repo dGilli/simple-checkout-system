c40ebb17d8308a84039016b90a1002911130243a
dGilli <me@dennisgilli.com>
2025-07-12 22:07:45 +0200

    fix indent to 4 in `src/` dir

--- src/PWABadge.jsx
+++ src/PWABadge.jsx
@@ -3,53 +3,53 @@ import './PWABadge.css'
 import { useRegisterSW } from 'virtual:pwa-register/react'
 
 function PWABadge() {
-  // check for updates every hour
-  const period = 60 * 60 * 1000
+    // check for updates every hour
+    const period = 60 * 60 * 1000
 
-  const {
-    
-    needRefresh: [needRefresh, setNeedRefresh],
-    updateServiceWorker,
-  } = useRegisterSW({
-    onRegisteredSW(swUrl, r) {
-      if (period <= 0) return
-      if (r?.active?.state === 'activated') {
-        registerPeriodicSync(period, swUrl, r)
-      }
-      else if (r?.installing) {
-        r.installing.addEventListener('statechange', (e) => {
-          /** @type {ServiceWorker} */
-          const sw = e.target
-          if (sw.state === 'activated')
-            registerPeriodicSync(period, swUrl, r)
-        })
-      }
-    },
-  })
+    const {
 
-  function close() {
-    
-    setNeedRefresh(false)
-  }
+        needRefresh: [needRefresh, setNeedRefresh],
+        updateServiceWorker,
+    } = useRegisterSW({
+        onRegisteredSW(swUrl, r) {
+            if (period <= 0) return
+            if (r?.active?.state === 'activated') {
+                registerPeriodicSync(period, swUrl, r)
+            }
+            else if (r?.installing) {
+                r.installing.addEventListener('statechange', (e) => {
+                    /** @type {ServiceWorker} */
+                    const sw = e.target
+                    if (sw.state === 'activated')
+                        registerPeriodicSync(period, swUrl, r)
+                })
+            }
+        },
+    })
 
-  return (
-    <div className="PWABadge" role="alert" aria-labelledby="toast-message">
-      { (needRefresh)
-      && (
-        <div className="PWABadge-toast">
-          <div className="PWABadge-message">
-            <span id="toast-message">New content available, click on reload button to update.</span>
-              
-              
-          </div>
-          <div className="PWABadge-buttons">
-            <button className="PWABadge-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button>
-            <button className="PWABadge-toast-button" onClick={() => close()}>Close</button>
-          </div>
+    function close() {
+
+        setNeedRefresh(false)
+    }
+
+    return (
+        <div className="PWABadge" role="alert" aria-labelledby="toast-message">
+            { (needRefresh)
+                && (
+                    <div className="PWABadge-toast">
+                        <div className="PWABadge-message">
+                            <span id="toast-message">New content available, click on reload button to update.</span>
+
+
+                        </div>
+                        <div className="PWABadge-buttons">
+                            <button className="PWABadge-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button>
+                            <button className="PWABadge-toast-button" onClick={() => close()}>Close</button>
+                        </div>
+                    </div>
+                )}
         </div>
-      )}
-    </div>
-  )
+    )
 }
 
 export default PWABadge
@@ -61,21 +61,21 @@ export default PWABadge
  * @param r {ServiceWorkerRegistration}
  */
 function registerPeriodicSync(period, swUrl, r) {
-  if (period <= 0) return
+    if (period <= 0) return
 
-  setInterval(async () => {
-    if ('onLine' in navigator && !navigator.onLine)
-      return
+    setInterval(async () => {
+        if ('onLine' in navigator && !navigator.onLine)
+            return
 
-    const resp = await fetch(swUrl, {
-      cache: 'no-store',
-      headers: {
-        'cache': 'no-store',
-        'cache-control': 'no-cache',
-      },
-    })
+        const resp = await fetch(swUrl, {
+            cache: 'no-store',
+            headers: {
+                'cache': 'no-store',
+                'cache-control': 'no-cache',
+            },
+        })
 
-    if (resp?.status === 200)
-      await r.update()
-  }, period)
+        if (resp?.status === 200)
+            await r.update()
+    }, period)
 }
