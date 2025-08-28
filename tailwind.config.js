2fbc06b8e86f1adb02e243070887687fb44fc829
dGilli <me@dennisgilli.com>
2025-08-28 22:34:09 +0200

    improve selected product item responsive layout on small screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
-export default {content: [
-  './index.html',
-  './src/**/*.{js,ts,jsx,tsx}'
-],}
+import containerQueriesPlugin from '@tailwindcss/container-queries'
+
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    plugins: [
+        containerQueriesPlugin,
+    ],
+}
+
